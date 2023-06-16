fa72a3f8f6ce8fdf85544e68838a93d7511ba43a
biaov06 <lvbiao@xcxd.com>
2023-06-16 13:42:10 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -17,7 +17,7 @@ const config: UserConfig = {
   plugins: [
     docsearchPlugin({
       appId: 'J4IXB1KJHT',
-      apiKey: 'bf1bfbfae3d460e0a5b4f5e7c9515ef2',
+      apiKey: 'dc49aef8f8c73afe86481f061d7ff175',
       indexName: 'wordpress-biaov',
       placeholder: '搜索文档',
       disableUserPersonalization: false,
@@ -26,7 +26,44 @@ const config: UserConfig = {
           placeholder: '搜索文档',
           translations: {
             button: {
-              buttonText: '搜索文档'
+              buttonText: 'Search',
+              buttonAriaLabel: 'Search'
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '重置',
+                resetButtonAriaLabel: '重置',
+                cancelButtonText: '取消',
+                cancelButtonAriaLabel: '取消'
+              },
+              startScreen: {
+                recentSearchesTitle: '历史',
+                noRecentSearchesText: '暂无内容',
+                saveRecentSearchButtonTitle: '保存',
+                removeRecentSearchButtonTitle: '移除历史记录',
+                favoriteSearchesTitle: '关注',
+                removeFavoriteSearchButtonTitle: '移除关注'
+              },
+              errorScreen: {
+                titleText: '错误',
+                helpText: '您可能需要检查您的网络连接！'
+              },
+              footer: {
+                selectText: '选择',
+                selectKeyAriaLabel: '回车',
+                navigateText: '导航',
+                navigateUpKeyAriaLabel: '上箭头',
+                navigateDownKeyAriaLabel: '下箭头',
+                closeText: '关闭',
+                closeKeyAriaLabel: '退出',
+                searchByText: '搜索方式'
+              },
+              noResultsScreen: {
+                noResultsText: '暂无内容',
+                suggestedQueryText: '尝试搜索',
+                reportMissingResultsText: '相信这个查询应该返回结果吗？',
+                reportMissingResultsLinkText: '让我们知道'
+              }
             }
           }
         }
