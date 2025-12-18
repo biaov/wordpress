543b05e9334f4f899fe7eccd8d94b66e2ee06e14
biaov <biaov@qq.com>
2025-12-18 11:36:05 +0800

    更新代码

--- materials/使视频显示为图片/CMD 命令 双击运行生成 3.png.bat
+++ materials/使视频显示为图片/CMD 命令 双击运行生成 3.png.bat
@@ -1,1 +1,6 @@
 copy /b 1.png + 2.zip 3.png
+
+@REM 还原方法 更改后缀名为 zip，7-zip 打开压缩包到 zip
+@REM 命令还原 假设知道图片大小 dir 1.png  // 90014
+@REM copy /b 3.png+,,+90014 recovered.zip // 待定
+@REM copy /b 3.png+,,+90014 recovered.png // 待定
